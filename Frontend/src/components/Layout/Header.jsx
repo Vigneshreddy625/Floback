51fac561957ae984ab995b9ca6e1de1aff28d2f4
Vigneshreddy <vigneshreddy625@gmail.com>
2025-08-05 14:36:39 +0530

    resolved account issue

--- Frontend/src/components/Layout/Header.jsx
+++ Frontend/src/components/Layout/Header.jsx
@@ -143,7 +143,7 @@ function Header() {
                 <DropdownMenuSeparator />
                 <DropdownMenuItem
                   className="cursor-pointer"
-                  onClick={() => navigate("/account/profile")}
+                  onClick={() => navigate("/account")}
                 >
                   <User className="h-4 w-4 mr-2" />
                   Account
